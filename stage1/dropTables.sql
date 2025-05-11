4b75e56adc51f325d2a6b85923ae0edaf294207f
RUBEN BENSIMON <133374168+ruben-28@users.noreply.github.com>
2025-05-11 18:37:41 +0300

    Add files via upload

---
+++ stage1/dropTables.sql
@@ -0,0 +1,9 @@
+-- Suppression dans l'ordre inverse des dépendances
+DROP TABLE TransactionAudit;
+DROP TABLE TransactionFee;
+DROP TABLE "Check";
+DROP TABLE Transfer;
+DROP TABLE "Transaction";
+DROP TABLE TransactionType;
+DROP TABLE Employee;
+DROP TABLE Account;
